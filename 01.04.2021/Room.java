a611ec6452839c5d78a8c2753e6f3343c0260c1d
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-04-01 21:28:07 +0300

    Create Room.java

---
+++ 01.04.2021/Room.java
@@ -0,0 +1,17 @@
+public class Room {
+    int place;
+    boolean shower;
+    boolean toilet;
+    boolean wifi;
+    boolean eat;
+    int roomNumber;
+
+    public Room(int place, boolean shower, boolean toilet, boolean wifi, boolean eat, int roomNumber) {
+        this.place = place;
+        this.shower = shower;
+        this.toilet = toilet;
+        this.wifi = wifi;
+        this.eat = eat;
+        this.roomNumber = roomNumber;
+    }
+}
